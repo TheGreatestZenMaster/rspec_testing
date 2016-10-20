265db1eeb503339396812eff1b99b57d0c86c467
TheGreatestZenMaster <jetloose@gmail.com>
2016-10-20 07:09:35 +0000

    Tests for Caesar's Cipher

--- caesars_cipher/spec/cipher_spec.rb
+++ caesars_cipher/spec/cipher_spec.rb
@@ -1,7 +1,16 @@
-require "caesers_cipher"
+require "caesars_cipher"
 
-describe ".caeser_cipher" do
-    context "respsonds to chiper" do
-        it { expect(caeser_cipher("", 0)).to eql("") }
+describe ".caesar_cipher" do
+    context "Preserves capitalization" do
+        it { expect(caesars_cipher("Hello", 1)).to eql("Ifmmp") }
+    end
+    context "preserves spaces" do
+        it {  expect(caesars_cipher("hello dog cat mouse", 1)).to eql("ifmmp eph dbu npvtf") } 
+    end
+    context "Preserves punctuations" do
+        it {  expect(caesars_cipher("Hello. I'm Jake!", 1)).to eql("Ifmmp. J'n Kblf!") } 
+    end
+    context "reverts to A/a when going past Z/z" do
+        it { expect(caesars_cipher("z Z", 1)).to eql("a A") }
     end
 end
